5aa35e1475445f9a9f9670a1f4c92a78a287f3ac
ramdan0cool <32842840+ramdan0cool@users.noreply.github.com>
2018-03-11 15:45:34 +0700

    A*
    Adjacency List antar kota

---
+++ A-star.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 11 15:26:51 2018
+
+@author: Ramdan
+"""
+
+cities={'Oradea':['Zerind', 'Sibiu'], 'Zerind':['Oradea', 'Arad'],
+        'Arad':['Zerind', 'Timisoara', 'Sibiu'], 
+        'Sibiu':['Arad', 'Fagaras', 'Rimnicu Vilcea', 'Oradea'], 
+        'Timisoara': ['Arad', 'Lugoj'],
+        'Fagaras':['Sibiu', 'Bucharest'],
+        'Rimnicu Vilcea':['Sibiu', 'Pitesti','Craiova'],
+        'Lugoj':['Timisoara', 'Mehadia'],
+        'Mehadia':['Dobreta', 'Lugoj'],
+        'Dobreta':['Craiova', 'Rimnicu Vilcea', 'Pitesti'],
+        'Pitesti':['Rimnicu Vilcea', 'Craiova', 'Bucharest'],
+        'Bucharest':['Giurgiu', 'Fagaras', 'Pitesti', 'Urziceni'],
+        'Giurgiu':['Bucharest'],
+        'Urziceni':['Vaslui', 'Hirsova'],
+        'Hirsova':['Urziceni', 'Eforie'],
+        'Eforie':['Hirsova'],
+        'Vaslui':['Urziceni', 'Iasi'],
+        'Iasi':['Neamt', 'Vaslui'],
+        'Neamt':['Iasi']}
